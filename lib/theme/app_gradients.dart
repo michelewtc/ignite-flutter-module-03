eaef19464ed2cdb20fa3d0d2157c73d94db57742
michelewtc <michelechan1996@gmail.com>
2021-05-28 17:26:42 -0300

    splashPage

---
+++ lib/theme/app_gradients.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppGradients {
+  Gradient get background;
+}
+
+class AppGradientsDefault implements AppGradients {
+  @override
+  Gradient get background => LinearGradient(colors: [
+        Color(0xFF40B38C),
+        Color(0xFF45CC93),
+      ], transform: GradientRotation(2.35619 * pi));
+}
