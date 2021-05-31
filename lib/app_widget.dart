fa167312970778b556972b859bdea330d85e53d7
Michelle <71111468+MiWChan@users.noreply.github.com>
2021-05-30 22:33:53 -0300

    created loginPage

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:split_it/modules/splash/splash_page.dart';
+import 'modules/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Split.it",
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:split_it/theme/app_theme.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.colors.backgroundPrimary,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 40),
+            child: Row(
+              children: [
+                Container(
+                  width: 236,
+                  child: Text(
+                    "Divida suas contas com seus amigos",
+                    style: GoogleFonts.montserrat(
+                        color: AppTheme.colors.title,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 40),
+                child: ListTile(
+                  leading: Image.asset("assets/images/emoji.png"),
+                  title: Text("Faça seu login com uma das contas abaixo",
+                      style: GoogleFonts.inter(
+                          color: AppTheme.colors.button,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400)),
+                ),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              ElevatedButton.icon(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          AppTheme.colors.backgroundPrimary)),
+                  onPressed: () {},
+                  icon: Image.asset("assets/images/google.png"),
+                  label: Text("Entrar com Google",
+                      style: GoogleFonts.inter(
+                          color: AppTheme.colors.button,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400))),
+              SizedBox(
+                height: 12,
+              ),
+              ElevatedButton.icon(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          AppTheme.colors.backgroundPrimary)),
+                  onPressed: () {},
+                  icon: Image.asset("assets/images/apple.png"),
+                  label: Text("Entrar com Apple",
+                      style: GoogleFonts.inter(
+                          color: AppTheme.colors.button,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400))),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
