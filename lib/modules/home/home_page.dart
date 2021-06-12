bda77b19b820bd295cdce831dec4706ae5385fbf
michelewtc <michelechan1996@gmail.com>
2021-06-11 21:27:15 -0300

    Loading with Shimmer & Module 06✔️

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:split_it/modules/home/home_state.dart';
 import 'package:split_it/modules/home/widgets/app_bar/app_bar_widget.dart';
 import 'package:split_it/modules/home/widgets/event_tile_widget.dart';
 import 'package:split_it/modules/login/models/user_model.dart';
+import 'package:split_it/shared/models/event_model.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -38,7 +39,10 @@ class _HomePageState extends State<HomePage> {
                 child: Column(
                   children: [
                     if (controller.state is HomeStateLoading) ...[
-                      CircularProgressIndicator(),
+                      ...List.generate(
+                          3,
+                          (index) => EventTileWidget(
+                              isLoading: true, model: EventModel()))
                     ] else if (controller.state is HomeStateSuccess) ...[
                       ...(controller.state as HomeStateSuccess)
                           .events
