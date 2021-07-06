c56755b5d85b0efcd4ad08327e826220a44402e5
michelewtc <michelechan1996@gmail.com>
2021-07-05 21:38:13 -0300

    feat: Mobx to LoginController

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,37 +1,27 @@
-import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
 
 import 'package:split_it/modules/login/login_service.dart';
 import 'package:split_it/modules/login/login_state.dart';
 
 class LoginController {
-  LoginState state = LoginStateEmpty();
-  VoidCallback onUpdate;
-  Function(LoginState state)? onChange;
+  final _state = Observable<LoginState>(LoginStateEmpty());
+  LoginState get state => _state.value;
+  set state(LoginState state) => _state.value = state;
 
   final LoginService service;
-  LoginController({required this.onUpdate, required this.service});
+  late Action googleSignIn;
+  LoginController({required this.service}) {
+    googleSignIn = Action(_googleSignIn);
+  }
 
-  Future<void> googleSignIn() async {
+  Future<void> _googleSignIn() async {
     try {
       state = LoginStateLoading();
-      update();
+
       final user = await service.googleSignIn();
       state = LoginStateSuccess(user: user);
-      update();
     } catch (error) {
       state = LoginStateFailure(message: error.toString());
-      update();
     }
-  }
-
-  void update() {
-    onUpdate();
-    if (onChange != null) {
-      onChange!(state);
-    }
-  }
-
-  void listen(Function(LoginState state) onChange) {
-    this.onChange = onChange;
   }
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:split_it/modules/login/login_controller.dart';
 import 'package:split_it/modules/login/login_service.dart';
 import 'package:split_it/modules/login/login_state.dart';
@@ -18,15 +19,18 @@ class _LoginPageState extends State<LoginPage> {
   @override
   void initState() {
     controller = LoginController(
-        service: LoginServiceImpl(),
-        onUpdate: () {
-          if (controller.state is LoginStateSuccess) {
-            final user = (controller.state as LoginStateSuccess).user;
-            Navigator.pushReplacementNamed(context, "/home", arguments: user);
-          } else {
-            setState(() {});
-          }
-        });
+      service: LoginServiceImpl(),
+    );
+
+    // {
+    //   if (controller.state is LoginStateSuccess) {
+    //     final user = (controller.state as LoginStateSuccess).user;
+    //     Navigator.pushReplacementNamed(context, "/home", arguments: user);
+    //   } else {
+    //     setState(() {});
+    //   }
+    // }
+
     super.initState();
   }
 
@@ -69,21 +73,22 @@ class _LoginPageState extends State<LoginPage> {
               SizedBox(
                 height: 32,
               ),
-              if (controller.state is LoginStateLoading) ...[
-                CircularProgressIndicator(),
-              ] else if (controller.state is LoginStateFailure) ...[
-                Text((controller.state as LoginStateFailure).message)
-              ] else
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32),
-                  child: SocialButtonWidget(
-                    imagePath: "assets/images/google.png",
-                    label: "Entrar com Google",
-                    onTap: () {
-                      controller.googleSignIn();
-                    },
-                  ),
-                ),
+              Observer(builder: (context) {
+                if (controller.state is LoginStateLoading)
+                  return CircularProgressIndicator();
+                else if (controller.state is LoginStateFailure)
+                  return Text((controller.state as LoginStateFailure).message);
+                else
+                  return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32),
+                      child: SocialButtonWidget(
+                        imagePath: "assets/images/google.png",
+                        label: "Entrar com Google",
+                        onTap: () {
+                          controller.googleSignIn();
+                        },
+                      ));
+              }),
               SizedBox(
                 height: 12,
               ),
