ffc60594f3a8e6d8a9a994ec2f2765ea420ea0bb
michelewtc <michelechan1996@gmail.com>
2021-07-08 23:57:53 -0300

    feat: Generating mobx code cleanly

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,21 +1,19 @@
 import 'package:mobx/mobx.dart';
-
 import 'package:split_it/modules/login/login_service.dart';
 import 'package:split_it/modules/login/login_state.dart';
 
-class LoginController {
-  final _state = Observable<LoginState>(LoginStateEmpty());
-  LoginState get state => _state.value;
-  set state(LoginState state) => _state.value = state;
+part 'login_controller.g.dart';
 
+class LoginController = _LoginControllerBase with _$LoginController;
+
+class _LoginControllerBase with Store {
   final LoginService service;
-  final _actionController = ActionController();
 
-  LoginController({required this.service});
+  _LoginControllerBase({required this.service});
+
+  LoginState state = LoginStateEmpty();
 
   Future<void> googleSignIn() async {
-    _actionController.startAction(name: "LoginController.googleSignIn");
-    final startTime = DateTime.now();
     try {
       state = LoginStateLoading();
       final user = await service.googleSignIn();
@@ -23,7 +21,5 @@ class LoginController {
     } catch (error) {
       state = LoginStateFailure(message: error.toString());
     }
-    _actionController.endAction(ActionRunInfo(
-        name: "LoginController.googleSignIn", startTime: startTime));
   }
 }
